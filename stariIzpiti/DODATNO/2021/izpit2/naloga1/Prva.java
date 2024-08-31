9d176eb40c42dbabb305f8df4f457ca6146b09a5
Martin Malenšek <mm53649@student.uni-lj.si>
2024-08-31 14:25:21 +0200

    Drugi izpit 2021

--- stariIzpiti/DODATNO/2021/izpit2/naloga1/Prva.java
+++ stariIzpiti/DODATNO/2021/izpit2/naloga1/Prva.java
@@ -8,6 +8,29 @@ import java.util.*;
 public class Prva {
 
     public static void main(String[] args) {
-        // dopolnite ...
+        int visinaFaktor, sirinaFaktor;
+
+        Scanner sc = new Scanner(System.in);
+
+        visinaFaktor = sc.nextInt();
+        sirinaFaktor = sc.nextInt();
+
+        for(int i = 0; i < 5 * visinaFaktor; i++){
+            for(int y = 0; y < 4 * sirinaFaktor; y++){
+                if(i < 2 * visinaFaktor || i >= 3 * visinaFaktor){
+                    if(y < sirinaFaktor || y >= 3 * sirinaFaktor){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }else{
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
--- stariIzpiti/DODATNO/2021/izpit2/naloga2/Druga.java
+++ stariIzpiti/DODATNO/2021/izpit2/naloga2/Druga.java
@@ -7,7 +7,45 @@ import java.util.*;
 
 public class Druga {
 
+    public static int visinaGore(int[][] tabela, int kateriStolpec){
+        int visina = tabela.length;
+        for(int i = 0; i < tabela.length; i++){
+            if(tabela[i][kateriStolpec] == 0){
+                visina--;
+            }
+        }
+        return visina; 
+    }
     public static void main(String[] args) {
-        // dopolnite ...
+        int visinaSlike, sirinaSlike;
+
+        Scanner sc = new Scanner(System.in);
+
+        visinaSlike  = sc.nextInt();
+        sirinaSlike = sc.nextInt();
+
+        int[][] tabela = new int[visinaSlike][sirinaSlike];
+
+        for(int i = 0; i < visinaSlike; i++){
+            for(int y = 0; y < sirinaSlike; y++){
+                tabela[i][y] = sc.nextInt();
+            }
+        }
+
+        int dvig = 0, spust = 0, trenutnaVisina = visinaGore(tabela, 0), prejsnaVisina;
+        for(int i = 1; i < tabela[0].length; i++){
+            prejsnaVisina = trenutnaVisina;
+            trenutnaVisina = visinaGore(tabela, i);
+            if(trenutnaVisina - prejsnaVisina > 0){
+                dvig += trenutnaVisina - prejsnaVisina;
+            }else{
+                spust += prejsnaVisina - trenutnaVisina;
+            }
+        }
+
+        System.out.println(dvig);
+        System.out.println(spust);
+
+        sc.close();
     }
 }
