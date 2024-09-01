10e0cb35c684ff4b5872e46a2dc4b4eae4526da3
Martin Malenšek <mm53649@student.uni-lj.si>
2024-09-01 10:16:23 +0200

    Dodano za učenje h Urši

--- stariIzpiti/DODATNO/2021/izpit3/naloga1/Prva.java
+++ stariIzpiti/DODATNO/2021/izpit3/naloga1/Prva.java
@@ -3,7 +3,35 @@ import java.util.*;
 
 public class Prva {
 
+    public static int vsotaStevk(int stevilka){
+        int vsota = 0;
+
+        while(stevilka > 0){
+            vsota += stevilka % 10;
+            stevilka /= 10;
+        }
+
+        return vsota;
+    }
+
     public static void main(String[] args) {
-        // dopolnite ...
+        int dolzina, vsotaStevk;
+
+        Scanner sc = new Scanner(System.in);
+
+        dolzina = sc.nextInt();
+        vsotaStevk = sc.nextInt();
+
+        int najmanjsi = (int) Math.pow(10, (double) dolzina - 1);
+        int najvecji = (int) Math.pow(10, (double) dolzina);
+        
+        for(int i = najmanjsi; i < najvecji; i++){
+            if(vsotaStevk(i) == vsotaStevk){
+                System.out.println(i);
+                break;
+            }
+        }
+    
+        sc.close();
     }
 }
--- stariIzpiti/DODATNO/2021/izpit3/naloga2/Druga.java
+++ stariIzpiti/DODATNO/2021/izpit3/naloga2/Druga.java
@@ -4,7 +4,10 @@ import java.util.*;
 public class Druga {
 
     public static int steviloShranjenih(int[] h, int[] w, int[] a, int[] b) {
-        // popravite / dopolnite ...
-        return -1;
+        int kolikoPredmetovDaNotr = 0;
+
+        
+
+        return kolikoPredmetovDaNotr;
     }
 }
